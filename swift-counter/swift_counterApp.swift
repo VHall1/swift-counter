08ce1f9a874ecffceb305505e00a621a259ef118
VHall1 <victorhallpsn@hotmail.com>
2024-10-30 12:37:41 +0000

    Initial Commit

---
+++ swift-counter/swift_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_counterApp.swift
+//  swift-counter
+//
+//  Created by Victor Hall on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
